78b466368101b01a328da232f24aa30ca4e86826
Kacper Michta <kacper.michta@itcentric.co.uk>
2020-03-02 10:21:45 +0000

    Simple Singular Neuron

--- Sakbot.py
+++ Sakbot.py
@@ -1,11 +1,22 @@
 import numpy as np
 
-test_set= np.array([[1,0,0],[0,1,0],[0,0,1],[0,1,1],[1,1,1]])
-expected_out= np.array([[1,1,0,0,0]])
-expected_out= expected_out.reshape(5,1)
+    # Defining Activation Function
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
 
-np.random.seed(15)
-weights = np.random.rand(3,1)
-bias = np.random.rand(1)
-lr = 0.05
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
 
+        # Performing step 1 feed forward process with inputs, weights, and activation function
+    def feedForward(self, inputs):
+        total = np.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
+
+weights = np.array([0,1])       # w1 = 0, w2 = 1
+bias = 4                        # b = 4
+n = Neuron(weights, bias)
+
+x = np.array([2,3])             # x1 = 2, x2 = 3
+print(n.feedForward(x))         # Outputs 0.9990
